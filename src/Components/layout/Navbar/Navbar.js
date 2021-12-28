324cd0e66d8262485f1ba1cf289fdc8c411d010d
Anand Simmy <anandsimmy7@gmail.com>
2021-12-28 10:06:50 +0530

    iptv changes

--- src/Components/layout/Navbar/Navbar.js
+++ src/Components/layout/Navbar/Navbar.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import logo from '../../../Assets/images/iptvlogo.png';
 import './Navbar.css';
 
 
 const Navbar = () => {
     return (
         <nav className='navbarWrapper'>
-            <span className='logoWrapper'></span>
+            <span className='logoWrapper'>
+                <img className='logoImage' src={logo} alt='Logo' />
+            </span>
             <div className='linksAndSearchWrapper'>
                 <div className='searchWrapper'></div>
                 <div className='linksWrapper'>
-                    <div className='linkItem'>TV Channels</div>
-                    <div className='linkItem'>Movies</div>
-                    <div className='linkItem'>More</div>
-                    <div className='linkItem'>About</div>
+                    <div tabIndex={0} className='linkItem'>TV Channels</div>
+                    <div tabIndex={0} className='linkItem'>Movies</div>
+                    <div tabIndex={0} className='linkItem'>More</div>
+                    <div tabIndex={0} className='linkItem'>About</div>
                 </div>
             </div>
         </nav>
--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,9 +1,54 @@
 import React from 'react';
 import Navbar from '../../Components/layout/Navbar/Navbar';
+import iptv1 from '../../Assets/images/img1.png';
+import iptv2 from '../../Assets/images/img2.png';
+import iptv3 from '../../Assets/images/img3.png';
+import iptv4 from '../../Assets/images/img4.png';
+import './Homepage.css';
 
 const Homepage = () => {
+
+    const getImageSrc = (index) => {
+        const imageIndex = (index % 4);
+        switch (imageIndex) {
+            case 0:
+                return iptv1;
+            case 1:
+                return iptv2;
+            case 2:
+                return iptv3;
+            default:
+                return iptv4
+        }
+    }
+
     return (
-        <Navbar />
+        <div className='homepageWrapper'>
+            <Navbar />
+            <section className='contentWrapper'>
+                <div className='channelListWrapper'>
+                    <ul className='channelList'>
+                        <li tabIndex={0} className='channelListItem'>All</li>
+                        <li tabIndex={0} className='channelListItem'>Kurdish</li>
+                        <li tabIndex={0} className='channelListItem'>Arabic</li>
+                        <li tabIndex={0} className='channelListItem'>Turkish</li>
+                        <li tabIndex={0} className='channelListItem'>English</li>
+                        <li tabIndex={0} className='channelListItem'>French</li>
+                    </ul>
+                </div>
+                <div className='channelsView'>
+                    {
+                        Array(18).fill(0).map((channelItem, index) => {
+                            return (
+                                <div className='channelItem'>
+                                    <img tabIndex={0} className='channelItemImage' src={getImageSrc(index)} alt='channelImage' />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </section>
+        </div>
     )
 }
 
