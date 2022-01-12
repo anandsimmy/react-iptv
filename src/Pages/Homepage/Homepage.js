c1c3e800948c4447015e085cfb64db63a9789634
Anand Simmy <anandsimmy7@gmail.com>
2022-01-12 11:12:45 +0530

    homepage changes

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -6,51 +6,71 @@ import iptv2 from '../../Assets/images/img2.png';
 import iptv3 from '../../Assets/images/img3.png';
 import iptv4 from '../../Assets/images/img4.png';
 import './Homepage.css';
+import { channelList } from '../../Assets/ChannelList';
+console.log('channelList', channelList);
 
 const Homepage = () => {
-
-    const getImageSrc = (index) => {
-        const imageIndex = (index % 4);
-        switch (imageIndex) {
-            case 0:
-                return iptv1;
-            case 1:
-                return iptv2;
-            case 2:
-                return iptv3;
-            default:
-                return iptv4
-        }
+  const getImageSrc = (index) => {
+    const imageIndex = index % 4;
+    switch (imageIndex) {
+      case 0:
+        return iptv1;
+      case 1:
+        return iptv2;
+      case 2:
+        return iptv3;
+      default:
+        return iptv4;
     }
+  };
 
-    return (
-        <div className='homepageWrapper'>
-            <Navbar />
-            <section className='contentWrapper'>
-                <div className='channelListWrapper'>
-                    <ul className='channelList'>
-                        <li tabIndex={0} className='channelListItem'>All</li>
-                        <li tabIndex={0} className='channelListItem'>Kurdish</li>
-                        <li tabIndex={0} className='channelListItem'>Arabic</li>
-                        <li tabIndex={0} className='channelListItem'>Turkish</li>
-                        <li tabIndex={0} className='channelListItem'>English</li>
-                        <li tabIndex={0} className='channelListItem'>French</li>
-                    </ul>
-                </div>
-                <div className='channelsView'>
-                    {
-                        Array(27).fill(0).map((channelItem, index) => {
-                            return (
-                                <Link to={`/channel?index=${index}`} key={index} className='channelItem'>
-                                    <img tabIndex={0} className='channelItemImage' src={getImageSrc(index)} alt='channelImage' />
-                                </Link>
-                            )
-                        })
-                    }
-                </div>
-            </section>
+  return (
+    <div className='homepageWrapper'>
+      <Navbar />
+      <section className='contentWrapper'>
+        <div className='channelListWrapper'>
+          <ul className='channelList'>
+            <li tabIndex={0} className='channelListItem'>
+              All
+            </li>
+            <li tabIndex={0} className='channelListItem'>
+              Kurdish
+            </li>
+            <li tabIndex={0} className='channelListItem'>
+              Arabic
+            </li>
+            <li tabIndex={0} className='channelListItem'>
+              Turkish
+            </li>
+            <li tabIndex={0} className='channelListItem'>
+              English
+            </li>
+            <li tabIndex={0} className='channelListItem'>
+              French
+            </li>
+          </ul>
         </div>
-    )
-}
+        <div className='channelsView'>
+          {channelList.items.map((channelItem, index) => {
+            return (
+              <Link
+                to={`/channel?channelUrl=${channelItem.url}`}
+                key={index}
+                className='channelItem'
+              >
+                <img
+                  tabIndex={0}
+                  className='channelItemImage'
+                  src={channelItem.tvg.logo}
+                  alt='channelImage'
+                />
+              </Link>
+            );
+          })}
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default Homepage;
