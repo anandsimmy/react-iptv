c7f2af73c74f8c5a00bc06c0654b0cc284c6a2df
Anand Simmy <anandsimmy7@gmail.com>
2022-02-19 11:49:09 +0530

    added event listener

--- src/Pages/ChannelStreaming/ChannelStreaming.js
+++ src/Pages/ChannelStreaming/ChannelStreaming.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import './ChannelStreaming.css';
@@ -7,13 +7,28 @@ const ChannelStreaming = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   console.log('searchParams', searchParams.get('channelUrl'));
 
+  useEffect(() => {
+    const backEvent = function (e) {
+      if (e.keyName === 'back') {
+        console.log('back event', e, window.location.href);
+        window.history.back();
+      }
+    };
+
+    // add eventListener for tizenhwkey (Back Button)
+    window.addEventListener('tizenhwkey', backEvent);
+    return () => {
+      window.removeEventListener('tizenhwkey', backEvent);
+    };
+  }, []);
+
   return (
     <div className='channelStreamingWrapper'>
       <ReactPlayer
         className='player-wrapper'
         url={
-          searchParams.get('channelUrl') ||
-          'http://210.210.155.35/qwr9ew/s/s50/index.m3u8'
+          !searchParams.get('channelUrl') ||
+          'http://forevertv.me:8080/movie/anand615/pass123/47041.mp4'
         }
         autoPlay
         playing
