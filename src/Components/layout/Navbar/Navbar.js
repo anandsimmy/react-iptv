e5359365a7eefcd79243ccb3a90dcb33c3a3db09
Anand Simmy <anandsimmy7@gmail.com>
2022-02-24 09:52:07 +0530

    naviagtion changes

--- src/Components/layout/Navbar/Navbar.js
+++ src/Components/layout/Navbar/Navbar.js
@@ -1,25 +1,45 @@
 import React from 'react';
 import logo from '../../../Assets/images/iptvlogo.png';
+import { Focusable, FocusableSection } from 'react-js-spatial-navigation';
 import './Navbar.css';
 
-
-const Navbar = () => {
-    return (
-        <nav className='navbarWrapper'>
-            <span className='logoWrapper'>
-                <img className='logoImage' src={logo} alt='Logo' />
-            </span>
-            <div className='linksAndSearchWrapper'>
-                <div className='searchWrapper'></div>
-                <div className='linksWrapper'>
-                    <div tabIndex={0} className='linkItem'>TV Channels</div>
-                    <div tabIndex={0} className='linkItem'>Movies</div>
-                    <div tabIndex={0} className='linkItem'>More</div>
-                    <div tabIndex={0} className='linkItem'>About</div>
-                </div>
-            </div>
-        </nav>
-    )
-}
+const Navbar = ({ filterHandler }) => {
+  return (
+    <nav className='navbarWrapper'>
+      <span className='logoWrapper'>
+        <img className='logoImage' src={logo} alt='Logo' />
+      </span>
+      <div className='linksAndSearchWrapper'>
+        <div className='searchWrapper'></div>
+        <div className='linksWrapper'>
+          <FocusableSection defaultElement='.tvChannels'>
+            <Focusable
+              onClickEnter={() => {
+                console.log('nav click 1');
+                filterHandler('channel');
+              }}
+              onFocus={() => {
+                console.log('nav focus 1');
+              }}
+            >
+              <div className='linkItem tvChannels'>TV Channels</div>
+            </Focusable>
+            <Focusable
+              onClickEnter={() => {
+                console.log('nav click 2');
+                filterHandler('movie');
+              }}
+              onFocus={() => {
+                console.log('nav focus 2');
+              }}
+            >
+              <div className='linkItem'>Movies</div>
+            </Focusable>
+          </FocusableSection>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
